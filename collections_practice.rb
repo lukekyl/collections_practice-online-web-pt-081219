30e3659d1a59fb2b672e6ffa20f4fd883823cc61
Kyle Luke <th.oldschool@gmail.com>
2019-08-21 23:57:23 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
 
+def sort_array_asc(array)
+  array.sort 
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+    tempa = array[1]
+    tempb = array[2]
+    array[1] = tempb
+    array[2] = tempa
+    array
+end
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array)
+  array.collect{|string|
+    string.split("")
+    string[2] = "$"
+    string
+  }
+end 
+
+def find_a(array)
+  temp = []
+  array.select{|string|
+    string.split("")
+    if string[0] == "a"
+      temp << string
+    end
+  }
+  return temp
+end
+
+def sum_array(array)
+  array.inject{|a, b| a + b }
+end 
+
+def add_s(array)
+  array.each_with_index.collect { |string, index|
+    if index != 1 
+      string << "s"
+    else
+      string
+    end
+  }
+end
